51b2340944a004a0aeca1a4c6332b9d1696b18db
bnorthan <bnorthan@gmail.com>
2024-03-27 17:44:41 -0400

    Delete template code

--- src/napari_segment_everything/__init__.py
+++ src/napari_segment_everything/__init__.py
@@ -3,11 +3,3 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
-
-__all__ = (
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
-)
